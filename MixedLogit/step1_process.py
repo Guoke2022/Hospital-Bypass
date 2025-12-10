b33734a42e3fab7797679e3014df88d906df7b05
panlongyan <1799402518@qq.com>
2025-12-10 12:47:40 +0800

    Update step1_process.py

--- MixedLogit/step1_process.py
+++ MixedLogit/step1_process.py
@@ -1,35 +1,33 @@
-
-# 导入相关的包
 import pandas as pd
 import numpy as np
 
 def process_data(data_path:str,out_path:str):
     
     """
-    处理数据
+    Process data
     """
 
-    # 读取数据
+    # load data
     df = pd.read_parquet(data_path)
 
-    print("完成数据读取")
+    print("load data")
     print(df.describe())
 
-    # id编码减少内存占用
+    # ID encoding
     df["id"] = df["id"].astype("category").cat.codes.astype("int32")
 
-    # 缺失值处理
+    # Missing value handling
     ids_with_missing = df[df.isnull().any(axis=1)]["id"].unique()
     df = df[~df["id"].isin(ids_with_missing)]
 
-    print("完成缺失值处理")
+    print("Missing value handling")
 
-    # SES重分组
+    # SES reclassification
     df["SES_Level"] = pd.qcut(df["SES"],q=[0,0.2,0.8,1],labels=["Low","Middle","High"])
 
-    print("完成分组处理")  
+    print("SES reclassification")  
 
-    # Label Encoding编码
+    # Label Encoding
     df["option_grade"] = df["option_grade"].map({
                             "0":0,
                             "Primary":1,
@@ -37,28 +35,28 @@ def process_data(data_path:str,out_path:str):
                             "Tertiary":3,
                             "Tertiary grade A":4}).astype("int8",copy=False)
 
-    print("完成Label Encoding编码处理") 
+    print("Label Encoding") 
 
-    # 单位变换
+    # Unit conversion
     df["option_distance_100_km"] = (df["option_distance_m"]/100_000).astype("float16")
     df["option_beds"] = (df["option_beds"]/100).astype("float16",copy=False)
 
-    print("完成单位变换处理") 
+    print("Unit conversion") 
 
-    # 添加距离二次项、三次项
+    # Add quadratic and cubic terms for distance
     df["option_distance_100_km_2"] = ((df["option_distance_100_km"]**2)).astype("float16")
     df["option_distance_100_km_3"] = ((df["option_distance_100_km"]**3)).astype("float16")
 
-    print("完成距离二次项添加处理")
+    print("Add quadratic and cubic terms for distance.")
 
-    # 清楚无用的项
+    # Clear unnecessary terms
     df.drop(columns=["option_distance_m","SES"],inplace=True)
 
-    # id和医院id排序
+    # Sorting
     df.sort_values(by=["id","option_hosp_id"],inplace=True)
-    print("id排序") 
+    print("Sorting") 
 
-    # 保存数据
+    # save data
     df.to_parquet(out_path)
 
 if __name__=="__main__":
